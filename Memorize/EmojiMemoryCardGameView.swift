40b9c562afbd57433bed32354b28168b7dc44b7a
martinideniam <martinideniam@gmail.com>
2022-04-15 00:41:42 +0300

    Code is up to date with Lecture 6 of CS193p ;)

--- Memorize/EmojiMemoryCardGameView.swift
+++ Memorize/EmojiMemoryCardGameView.swift
@@ -34,8 +34,13 @@ struct CardView: View {
             ZStack {
                 let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
                     if card.isFaceUp {
+                        
                         shape.fill(Color.white)
                         shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+                        Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 90-90), clockwise: false)
+                            .foregroundColor(.red)
+                            .opacity(0.4)
+                            .padding(5)
                         Text(card.content).font(returnFontSize(in: geometry.size))
                     } else {
                         shape.fill(Color.pink)
@@ -50,7 +55,7 @@ struct CardView: View {
     
     private struct DrawingConstants {
         static let cornerRadius:CGFloat = 10
-        static let fontScale = 0.6
+        static let fontScale = 0.65
         static let lineWidth:CGFloat = 3
     }
 }
@@ -61,7 +66,7 @@ struct CardView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         let game = EmojiMemoryGame()
-        EmojiMemoryCardGameView(game: game).preferredColorScheme(.light)
-        EmojiMemoryCardGameView(game: game).preferredColorScheme(.dark)
+        game.choose(game.cards.first!)
+        return EmojiMemoryCardGameView(game: game)
     }
 }
